d67dea47c339b829a611f92b8095c49e4385d747
Michael Bell <michaelrbell@ymail.com>
2026-02-15 10:29:13 +0000

    general: tidy version

--- version/version.go
+++ version/version.go
@@ -1,39 +1,30 @@
 package version
 
-import "strings"
-
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-// These variables are set via ldflags during the build
+// The following variables are set via ldflags during a build
 var (
-	// Version is the git tag (e.g., "v0.0.1") or "dev" for dev builds
+	// Version of the build (dev or a release tag)
 	Version = "dev"
 
-	// Commit is the short git commit hash
+	// Commit hash of the build
 	Commit = "unknown"
 )
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-// GetVersion returns the version string
-// For dev builds (when Version is "dev" or empty), it returns "dev-[commit-hash]"
-// For release builds, it returns the git tag
+// GetVersion returns a version string
 func GetVersion() string {
-	// If version is "dev" and we have a commit hash, return "dev-{commit}"
-	if Version == "dev" {
+	// When the version is empty or dev return 'dev-commit' or just 'dev' if the
+	// commit is unknown
+	if Version == "" || Version == "dev" {
 		if Commit != "unknown" && Commit != "" {
 			return "dev-" + Commit
 		}
+
 		return "dev"
 	}
-	// If version is empty, try to use commit hash
-	if Version == "" {
-		if Commit != "unknown" && Commit != "" {
-			return "dev-" + Commit
-		}
-		return "dev"
-	}
-	// Otherwise return the version (which should be a tag)
+
 	return Version
 }
 
@@ -42,11 +33,4 @@ func GetVersion() string {
 // GetCommit returns the commit hash
 func GetCommit() string {
 	return Commit
-}
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// IsDev returns true if this is a dev build
-func IsDev() bool {
-	return Version == "dev" || strings.HasPrefix(Version, "dev-")
 }
